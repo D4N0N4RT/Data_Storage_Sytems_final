5263024c0f683738cb4cb8ebabd1e7afed95450e
Daniil <d.ar57913@gmail.com>
2025-10-18 11:37:24 +0300

    some fixes

--- create_tables.sql
+++ create_tables.sql
@@ -1,4 +1,4 @@
-CREATE TABLE HUB_ORDERS (
+CREATE TABLE final.HUB_ORDERS (
     H_Hash_Order      VARCHAR(32)   NOT NULL, 
     Order_id          UUID          NOT NULL, 
     H_Load_Source    VARCHAR(50)   NOT NULL, 
@@ -6,7 +6,7 @@ CREATE TABLE HUB_ORDERS (
     CONSTRAINT pk_orders PRIMARY KEY (H_Hash_Order)
 );
 
-CREATE TABLE HUB_CUSTOMES (
+CREATE TABLE final.HUB_CUSTOMES (
     H_Hash_Customer      VARCHAR(32)   NOT NULL, 
     Customer_id          UUID          NOT NULL, 
     H_Load_Source    VARCHAR(50)   NOT NULL, 
@@ -14,7 +14,7 @@ CREATE TABLE HUB_CUSTOMES (
     CONSTRAINT pk_customers PRIMARY KEY (H_Hash_Customer)
 );
 
-CREATE TABLE HUB_SHIPMENTS (
+CREATE TABLE final.HUB_SHIPMENTS (
     H_Hash_Shipment      VARCHAR(32)   NOT NULL, 
     Shipment_id          UUID          NOT NULL, 
     H_Load_Source    VARCHAR(50)   NOT NULL,
@@ -22,7 +22,7 @@ CREATE TABLE HUB_SHIPMENTS (
     CONSTRAINT pk_shipments PRIMARY KEY (H_Hash_Shipment)
 );
 
-CREATE TABLE HUB_PRODUCTS (
+CREATE TABLE final.HUB_PRODUCTS (
     H_Hash_Product      VARCHAR(32)   NOT NULL, 
     Product_id          UUID          NOT NULL, 
     H_Load_Source    VARCHAR(50)   NOT NULL, 
@@ -30,50 +30,43 @@ CREATE TABLE HUB_PRODUCTS (
     CONSTRAINT pk_products PRIMARY KEY (H_Hash_Product)
 );
 
-CREATE TABLE LINK_CUSTOMER_ORDER (
+CREATE TABLE final.LINK_CUSTOMER_ORDER (
     L_Customer_Order_HK      VARCHAR(32)   NOT NULL, 
     H_Customer_HK      VARCHAR(32)   NOT NULL,
     H_Order_HK      VARCHAR(32)   NOT NULL,
     H_Load_Source    VARCHAR(50)   NOT NULL, 
     H_Load_Date      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_customer_order PRIMARY KEY (L_Customer_Order_HK),
-   	CONSTRAINT fk_customer_order_customer FOREIGN KEY (H_Customer_HK) REFERENCES HUB_CUSTOMERS(H_Hash_Customer),
-    CONSTRAINT fk_customer_order_order FOREIGN KEY (H_Order_HK) REFERENCES HUB_ORDERS(H_Hash_Order)
+    CONSTRAINT pk_customer_order PRIMARY KEY (L_Customer_Order_HK)
 );
 
-CREATE TABLE student56.LINK_ORDER_PRODUCT (
+CREATE TABLE final.LINK_ORDER_PRODUCT (
     L_Order_Product_HK      VARCHAR(32)   NOT NULL, 
     H_Order_HK      VARCHAR(32)   NOT NULL,
     H_Product_HK      VARCHAR(32)   NOT NULL,
     H_Load_Source    VARCHAR(50)   NOT NULL, 
     H_Load_Date      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_order_product PRIMARY KEY (L_Order_Product_HK),
-   	CONSTRAINT fk_order_product_order FOREIGN KEY (H_Order_HK) REFERENCES HUB_ORDERS(H_Hash_Order),
-    CONSTRAINT fk_order_product_product FOREIGN KEY (H_Product_HK) REFERENCES HUB_PRODUCTS(H_Hash_Product)
+    CONSTRAINT pk_order_product PRIMARY KEY (L_Order_Product_HK)
 );
 
-CREATE TABLE student56.LINK_ORDER_SHIPMENT (
+CREATE TABLE final.LINK_ORDER_SHIPMENT (
     L_Order_Shipment_HK      VARCHAR(32)   NOT NULL, 
     H_Order_HK      VARCHAR(32)   NOT NULL,
     H_Shipment_HK      VARCHAR(32)   NOT NULL,
     H_Load_Source    VARCHAR(50)   NOT NULL, 
     H_Load_Date      TIMESTAMP     NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT pk_order_shipment PRIMARY KEY (L_Order_Shipment_HK),
-   	CONSTRAINT fk_order_shipment_order FOREIGN KEY (H_Order_HK) REFERENCES HUB_ORDERS(H_Hash_Order),
-    CONSTRAINT fk_order_shipment_shipment FOREIGN KEY (H_Shipment_HK) REFERENCES HUB_SHIPMENTS(H_Hash_Shipment)
+    CONSTRAINT pk_order_shipment PRIMARY KEY (L_Order_Shipment_HK)
 );
 
-CREATE TABLE SATELITE_CUSTOMER_SEGMENT (
+CREATE TABLE final.SATELITE_CUSTOMER_SEGMENT (
     H_Customer_HK VARCHAR(32) NOT NULL,
     Load_Date TIMESTAMP NOT NULL,
     Segment TEXT,
     Hash_Diff TEXT,
     Load_Source TEXT,
-    CONSTRAINT pk_customer_segment PRIMARY KEY (H_Customer_HK, Load_Date),
-    CONSTRAINT fk_customer_segment_customer FOREIGN KEY (H_Customer_HK) REFERENCES HUB_CUSTOMERS(H_Hash_Customer)
+    CONSTRAINT pk_customer_segment PRIMARY KEY (H_Customer_HK, Load_Date)
 ) DISTRIBUTED REPLICATED;
 
-CREATE TABLE SATELITE_CUSTOMER_LOCATION (
+CREATE TABLE final.SATELITE_CUSTOMER_LOCATION (
     H_Customer_HK VARCHAR(32) NOT NULL,
     Load_Date TIMESTAMP NOT NULL,
     Postal_Code TEXT,
@@ -83,24 +76,22 @@ CREATE TABLE SATELITE_CUSTOMER_LOCATION (
     City TEXT,
     Hash_Diff TEXT,
     Load_Source TEXT,
-    CONSTRAINT pk_customer_location PRIMARY KEY (H_Customer_HK, Load_Date),
-    CONSTRAINT fk_customer_location_customer FOREIGN KEY (H_Customer_HK) REFERENCES HUB_CUSTOMERS(H_Hash_Customer)
+    CONSTRAINT pk_customer_location PRIMARY KEY (H_Customer_HK, Load_Date)
 ) DISTRIBUTED REPLICATED;
 
 
-CREATE TABLE SATELITE_PRODUCT_DETAILS (
+CREATE TABLE final.SATELITE_PRODUCT_DETAILS (
     H_Product_HK VARCHAR(32) NOT NULL,
     Load_Date TIMESTAMP NOT NULL,
     Category TEXT,
     Sub_Category TEXT,
     Hash_Diff TEXT,
     Load_Source TEXT,
-    CONSTRAINT pk_product_details PRIMARY KEY (H_Product_HK, Load_Date),
-    CONSTRAINT fk_product_detailSATELITE_product FOREIGN KEY (H_Product_HK) REFERENCES HUB_PRODUCTS(H_Hash_Product)
+    CONSTRAINT pk_product_details PRIMARY KEY (H_Product_HK, Load_Date)
 ) DISTRIBUTED REPLICATED;
 
 
-CREATE TABLE SATELITE_ORDER_PRODUCT_DETAILS (
+CREATE TABLE final.SATELITE_ORDER_PRODUCT_DETAILS (
     L_Order_Product_HK VARCHAR(32) NOT NULL,
     Load_Date TIMESTAMP NOT NULL,
     Quantity numeric,
--- data_load.py
+++ data_load.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 import hashlib
 
 engine = create_engine(
-    'postgresql+psycopg2://dan_art:766-jbv-3dS-CUi@rc1a-04r7nqsrbuot8pt1.mdb.yandexcloud.net,rc1a-9jvrmnobd72fakgo.mdb.yandexcloud.net:6432/postgres',
+    'postgresql+psycopg2://dan_art:766-jbv-3dS-CUi@rc1a-04r7nqsrbuot8pt1.mdb.yandexcloud.net,rc1a-9jvrmnobd72fakgo.mdb.yandexcloud.net:6432/final_task_db',
     connect_args={
         'application_name': 'data_load',
         'connect_timeout': 10
@@ -22,7 +22,7 @@ df_customer['H_Load_Date'] = pd.Timestamp.now()
 df_customer.to_sql(
     'HUB_CUSTOMERS',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi' 
@@ -38,7 +38,7 @@ df_order['H_Load_Date'] = pd.Timestamp.now()
 df_order.to_sql(
     'HUB_ORDERS',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
@@ -54,7 +54,7 @@ df_shipment['H_Load_Date'] = pd.Timestamp.now()
 df_shipment.to_sql(
     'HUB_SHIPMENTS',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
@@ -70,112 +70,112 @@ df_product['H_Load_Date'] = pd.Timestamp.now()
 df_product.to_sql(
     'HUB_PRODUCTS',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
 
-df_link_customer_order = df[['Customer_id', 'Order_id']].drop_duplicates()
-df_link_customer_order['L_Customer_Order_HK'] = df_link_customer_order.apply(
+df_customer_order = df[['Customer_id', 'Order_id']].drop_duplicates()
+df_customer_order['L_Customer_Order_HK'] = df_customer_order.apply(
     lambda row: hashlib.md5(f"{row['Customer_id']}_{row['Order_id']}".encode()).hexdigest(),
     axis=1
 )
 
 customer_hash_dict = df_customer.set_index('Customer_id')['H_Hash_Customer'].to_dict()
 orders_hash_dict = df_order.set_index('Order_id')['H_Hash_Order'].to_dict()
-df_link_customer_order['H_Customer_HK'] = df_link_customer_order['Customer_id'].map(customer_hash_dict)
-df_link_customer_order['H_Order_HK'] = df_link_customer_order['Order_id'].map(orders_hash_dict)
+df_customer_order['H_Customer_HK'] = df_customer_order['Customer_id'].map(customer_hash_dict)
+df_customer_order['H_Order_HK'] = df_customer_order['Order_id'].map(orders_hash_dict)
 
-df_link_customer_order['H_Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
-df_link_customer_order['H_Load_Date'] = pd.Timestamp.now()
+df_customer_order['H_Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
+df_customer_order['H_Load_Date'] = pd.Timestamp.now()
 
-df_link_final = df_link_customer_order[[
+df_final = df_customer_order[[
     'L_Customer_Order_HK',
     'H_Customer_HK',
     'H_Order_HK',
     'H_Load_Source'
 ]].drop_duplicates()
 
-df_link_final.to_sql(
+df_final.to_sql(
     'LINK_CUSTOMER_ORDER',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
 
-df_link_order_product = df[['Order_id', 'Product_id']].drop_duplicates()
-df_link_order_product['L_Order_Product_HK'] = df_link_order_product.apply(
+df_order_product = df[['Order_id', 'Product_id']].drop_duplicates()
+df_order_product['L_Order_Product_HK'] = df_order_product.apply(
     lambda row: hashlib.md5(f"{row['Order_id']}_{row['Product_id']}".encode()).hexdigest(),
     axis=1
 )
 
 product_hash_dict = df_product.set_index('Product_id')['H_Hash_Product'].to_dict()
 
-df_link_order_product['H_Order_HK'] = df_link_order_product['Order_id'].map(orders_hash_dict)
-df_link_order_product['H_Product_HK'] = df_link_order_product['Product_id'].map(product_hash_dict)
+df_order_product['H_Order_HK'] = df_order_product['Order_id'].map(orders_hash_dict)
+df_order_product['H_Product_HK'] = df_order_product['Product_id'].map(product_hash_dict)
 
-df_link_order_product['H_Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
-df_link_order_product['H_Load_Date'] = pd.Timestamp.now()
+df_order_product['H_Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
+df_order_product['H_Load_Date'] = pd.Timestamp.now()
 
-df_link_final = df_link_order_product[[
+df_final = df_order_product[[
     'L_Order_Product_HK',
     'H_Order_HK',
     'H_Product_HK',
     'H_Load_Source'
 ]].drop_duplicates()
 
-df_link_final.to_sql(
+df_final.to_sql(
     'LINK_ORDER_PRODUCT',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
 
-df_link_order_shipment = df[['Order_id', 'Shipment_id']].drop_duplicates()
-df_link_order_shipment['L_Order_Shipment_HK'] = df_link_order_shipment.apply(
+df_order_shipment = df[['Order_id', 'Shipment_id']].drop_duplicates()
+df_order_shipment['L_Order_Shipment_HK'] = df_order_shipment.apply(
     lambda row: hashlib.md5(f"{row['Order_id']}_{row['Shipment_id']}".encode()).hexdigest(),
     axis=1
 )
 
 shipment_hash_dict = df_shipment.set_index('Shipment_id')['H_Hash_Shipment'].to_dict()
 
-df_link_order_shipment['H_Order_HK'] = df_link_order_shipment['Order_id'].map(orders_hash_dict)
-df_link_order_shipment['H_Shipment_HK'] = df_link_order_shipment['Shipment_id'].map(product_hash_dict)
+df_order_shipment['H_Order_HK'] = df_order_shipment['Order_id'].map(orders_hash_dict)
+df_order_shipment['H_Shipment_HK'] = df_order_shipment['Shipment_id'].map(product_hash_dict)
 
-df_link_order_shipment['H_Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
-df_link_order_shipment['H_Load_Date'] = pd.Timestamp.now()
+df_order_shipment['H_Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
+df_order_shipment['H_Load_Date'] = pd.Timestamp.now()
 
-df_link_final = df_link_order_shipment[[
+df_final = df_order_shipment[[
     'L_Order_Shipment_HK',
     'H_Order_HK',
     'H_Shipment_HK',
     'H_Load_Source'
 ]].drop_duplicates()
 
-df_link_final.to_sql(
+df_final.to_sql(
     'LINK_ORDER_SHIPMENT',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
 
-df_s_customer_segment = df[['Customer_id', 'Segment']].drop_duplicates()
-df_s_customer_segment['H_Customer_HK'] = df_s_customer_segment['Customer_id'].map(customer_hash_dict)
-df_s_customer_segment['Hash_Diff'] = df_s_customer_segment[['Segment']].apply(
+df_customer_segment = df[['Customer_id', 'Segment']].drop_duplicates()
+df_customer_segment['H_Customer_HK'] = df_customer_segment['Customer_id'].map(customer_hash_dict)
+df_customer_segment['Hash_Diff'] = df_customer_segment[['Segment']].apply(
     lambda row: hashlib.md5('|'.join(row.astype(str)).encode()).hexdigest(),
     axis=1
 )
-df_s_customer_segment['Load_Date'] = pd.Timestamp.now()
-df_s_customer_segment['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
+df_customer_segment['Load_Date'] = pd.Timestamp.now()
+df_customer_segment['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
 
-df_s_customer_segment_final = df_s_customer_segment[[
+df_customer_segment_final = df_customer_segment[[
     'H_Customer_HK',
     'Load_Date',
     'Segment',
@@ -183,26 +183,26 @@ df_s_customer_segment_final = df_s_customer_segment[[
     'Load_Source'
 ]].drop_duplicates()
 
-df_s_customer_segment_final.to_sql(
+df_customer_segment_final.to_sql(
     'SATELITE_CUSTOMER_SEGMENT',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
 
 location_columns = ['Customer_id', 'Postal Code', 'Country', 'Region', 'State', 'City']
-df_s_customer_location = df[location_columns].drop_duplicates()
-df_s_customer_location['H_Customer_HK'] = df_s_customer_location['Customer_id'].map(customer_hash_dict)
-df_s_customer_location['Hash_Diff'] = df_s_customer_location[['Postal Code', 'Country', 'Region', 'State', 'City']].apply(
+df_customer_location = df[location_columns].drop_duplicates()
+df_customer_location['H_Customer_HK'] = df_customer_location['Customer_id'].map(customer_hash_dict)
+df_customer_location['Hash_Diff'] = df_customer_location[['Postal Code', 'Country', 'Region', 'State', 'City']].apply(
     lambda row: hashlib.md5('|'.join(row.astype(str)).encode()).hexdigest(),
     axis=1
 )
-df_s_customer_location['Load_Date'] = pd.Timestamp.now()
-df_s_customer_location['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
+df_customer_location['Load_Date'] = pd.Timestamp.now()
+df_customer_location['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
 
-df_s_customer_location_final = df_s_customer_location[[
+df_customer_location_final = df_customer_location[[
     'H_Customer_HK',
     'Load_Date',
     'Postal Code',
@@ -213,27 +213,27 @@ df_s_customer_location_final = df_s_customer_location[[
     'Hash_Diff',
     'Load_Source'
 ]].drop_duplicates()
-df_s_customer_location_final = df_s_customer_location_final.rename(columns={'Postal Code': 'Postal_Code'})
+df_customer_location_final = df_customer_location_final.rename(columns={'Postal Code': 'Postal_Code'})
 
-df_s_customer_location_final.to_sql(
+df_customer_location_final.to_sql(
     'SATELITE_CUSTOMER_LOCATION',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
 
 
-df_s_product_details = df[['Product_id', 'Category', 'Sub-Category']].drop_duplicates()
-df_s_product_details['H_Product_HK'] = df_s_product_details['Product_id'].map(product_hash_dict)
-df_s_product_details['Hash_Diff'] = df_s_product_details[['Category', 'Sub-Category']].apply(
+df_product_details = df[['Product_id', 'Category', 'Sub-Category']].drop_duplicates()
+df_product_details['H_Product_HK'] = df_product_details['Product_id'].map(product_hash_dict)
+df_product_details['Hash_Diff'] = df_product_details[['Category', 'Sub-Category']].apply(
     lambda row: hashlib.md5('|'.join(row.astype(str)).encode()).hexdigest(),
     axis=1
 )
-df_s_product_details['Load_Date'] = pd.Timestamp.now()
-df_s_product_details['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
-df_s_product_details_final = df_s_product_details[[
+df_product_details['Load_Date'] = pd.Timestamp.now()
+df_product_details['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
+df_product_details_final = df_product_details[[
     'H_Product_HK',
     'Load_Date',
     'Category',
@@ -242,29 +242,29 @@ df_s_product_details_final = df_s_product_details[[
     'Load_Source'
 ]].drop_duplicates()
 
-df_s_product_details_final = df_s_product_details_final.rename(columns={'Sub-Category': 'Sub_Category'})
+df_product_details_final = df_product_details_final.rename(columns={'Sub-Category': 'Sub_Category'})
 
-df_s_product_details_final.to_sql(
+df_product_details_final.to_sql(
     'SATELITE_PRODUCT_DETAILS',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
 
-df_s_order_product_details = df[['Order_id', 'Product_id', 'Quantity', 'Orders', 'Discount', 'Profit']].drop_duplicates()
-df_s_order_product_details['L_Order_Product_HK'] = df_s_order_product_details.apply(
+df_order_product_details = df[['Order_id', 'Product_id', 'Quantity', 'Orders', 'Discount', 'Profit']].drop_duplicates()
+df_order_product_details['L_Order_Product_HK'] = df_order_product_details.apply(
     lambda row: hashlib.md5(f"{row['Order_id']}_{row['Product_id']}".encode()).hexdigest(),
     axis=1
 )
-df_s_order_product_details['Hash_Diff'] = df_s_order_product_details[['Quantity', 'Orders', 'Discount', 'Profit']].apply(
+df_order_product_details['Hash_Diff'] = df_order_product_details[['Quantity', 'Orders', 'Discount', 'Profit']].apply(
     lambda row: hashlib.md5('|'.join(row.astype(str)).encode()).hexdigest(),
     axis=1
 )
-df_s_order_product_details['Load_Date'] = pd.Timestamp.now()
-df_s_order_product_details['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
-df_s_order_product_details_final = df_s_order_product_details[[
+df_order_product_details['Load_Date'] = pd.Timestamp.now()
+df_order_product_details['Load_Source'] = 'SampleSuperstoreWithEntityKeys.csv'
+df_order_product_details_final = df_order_product_details[[
     'L_Order_Product_HK',
     'Load_Date',
     'Quantity',
@@ -275,13 +275,11 @@ df_s_order_product_details_final = df_s_order_product_details[[
     'Load_Source'
 ]].drop_duplicates()
 
-df_s_order_product_details_final.to_sql(
+df_order_product_details_final.to_sql(
     'SATELITE_ORDER_PRODUCT_DETAILS',
     con=engine,
-    schema='student56',
+    schema='final',
     if_exists='append',
     index=False,
     method='multi'
 )
-
-print("Данные успешно загружены в SATELITE_ORDER_PRODUCT_DETAILS")
